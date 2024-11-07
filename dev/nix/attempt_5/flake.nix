3544b84eb001da791cfe2d04c030b9622e459e25
Reliable Magician <petr.b.lavrov@gmail.com>
2024-11-07 14:21:49 +0100

    Dump changes

--- dev/nix/attempt_5/flake.nix
+++ dev/nix/attempt_5/flake.nix
@@ -3,13 +3,14 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-stable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager }: {
+  outputs = { self, darwin, nixpkgs, nixpkgs-stable, home-manager }: {
     darwinConfigurations.Petrs-MacBook-Pro = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [ 
